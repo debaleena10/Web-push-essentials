5070d92535a0b4b249c3988d1daa037b68cade41
Debaleena <debaleena.guha@bik.ai>
2024-10-01 22:06:13 +0530

    first push

---
+++ bik-webpush.js
@@ -0,0 +1,118 @@
+self.addEventListener('notificationclick', function (event) {
+    console.log('Received notificationclick event');
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+  
+    var raw = JSON.stringify({
+      "eventName": "clicked",
+      "properties": {
+        "openedAt": new Date().toISOString()
+      },
+      "storeUrl": self.location.host,
+      "broadcastId": event.notification.data.broadcastId,
+      "customerId": event.notification.data.customerId
+    });
+  
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+  
+    fetch(`${event.notification.data.baseUrl}/webPushApiFunctions-captureEvent`, requestOptions)
+      .then(response => response.text())
+      .catch(error => console.log('error', error));
+    if (!event.action) {
+      var click_action = event.notification.data.url || event.notification.data;
+      event.notification.close();
+      event.waitUntil(clients.matchAll({
+        type: 'window'
+      }).then(function (clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+          var client = clientList[i];
+          if (client.url == click_action && 'focus' in client) { return client.focus(); }
+        }
+        if (clients.openWindow) { return clients.openWindow(click_action); }
+      }));
+      return;
+    }
+  
+    clients.openWindow(event.action);
+  
+  });
+  
+  importScripts('https://bikayi-chat.firebaseapp.com/__/firebase/9.2.0/firebase-app-compat.js');
+  importScripts('https://bikayi-chat.firebaseapp.com/__/firebase/9.2.0/firebase-messaging-compat.js');
+  importScripts('https://bikayi-chat.firebaseapp.com/__/firebase/init.js');
+  
+  const messaging = firebase.messaging();
+  
+  function captureMessageReceiveEvent(notificationOptions) {
+  
+    const eventsOnDelivered = ["delivered", "read"];
+    eventsOnDelivered.forEach(eventName => {
+      const payload = {
+        eventName,
+        properties: {
+          openedAt: new Date().toISOString()
+        },
+        storeUrl: self.location.host,
+        broadcastId: notificationOptions.data.broadcastId,
+        customerId: notificationOptions.data.customerId
+      }
+      const deliveredRaw = JSON.stringify(payload);
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+    
+      const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: deliveredRaw,
+        redirect: 'follow'
+      };
+    
+      fetch(notificationOptions.data.baseUrl + "/webPushApiFunctions-captureEvent", requestOptions)
+        .then(response => response.text())
+        .catch(error => console.log('error', error));
+    });
+  }
+  
+  messaging.onBackgroundMessage(function (payload) {
+    const actions = JSON.parse(payload.data['actions']);
+    const broadcastId = parseInt(payload.data['broadcast_id']);
+    const customerId = parseInt(payload.data['customer_id']);
+    const baseUrl = `${payload.data['base_url']}`
+  
+    const notificationTitle = payload.data.title;
+    const notificationOptions = {
+      body: payload.data.body,
+      icon: payload.data.icon,
+      image: payload.data.image,
+      title: payload.data.title,
+      data: {
+        url: payload.data.click_action,
+        broadcastId,
+        customerId,
+        baseUrl
+      },
+      actions,
+    };
+    captureMessageReceiveEvent(notificationOptions);
+    if (!actions) {
+      if (!("Notification" in window)) {
+        console.log("This browser does not support system notifications.");
+      } else if (Notification.permission === "granted") {
+        var notification = new Notification(notificationTitle, notificationOptions);
+        notification.onclick = function (event) {
+          event.preventDefault();
+          clients.openWindow(payload.fcmOptions.link, '_blank');
+          notification.close();
+        }
+      }
+      return;
+    }
+  
+    self.registration.showNotification(notificationTitle, notificationOptions)
+  })
+  
---
+++ firebase-setup.js
@@ -0,0 +1,4 @@
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { initializeApp } from "firebase/app";
+
+export { getMessaging, getToken, onMessage, initializeApp };
