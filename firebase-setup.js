b347dce50bcccaf449a28c99158ce89147366948
Debaleena <debaleena.guha@bik.ai>
2024-10-03 11:40:42 +0530

    change the global obkect

--- firebase-setup.js
+++ firebase-setup.js
@@ -1,15 +1,15 @@
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import { initializeApp } from "firebase/app";
 
-window.firebaseUtils = {
+window.firebase = {
   getMessaging,
   getToken,
   onMessage,
   initializeApp,
 };
 
-window.firebaseUtils.initializeFirebase = function(config) {
+window.firebase.initializeFirebase = function(config) {
   return initializeApp(config);
 };
 
-console.log("firebaseUtils is available on window:", window.firebaseUtils);
+console.log("firebaseUtils is available on window:", window.firebase);
