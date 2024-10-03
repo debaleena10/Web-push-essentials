30f04f385591117f258e986fab0eae33b95b3035
Debaleena <debaleena.guha@bik.ai>
2024-10-03 11:50:41 +0530

    utils bundle

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',  // 'development' for unminified output or 'production' for optimized bundle
   entry: './firebase-setup.js',  // Entry point of your Firebase setup file
   output: {
-    filename: 'firebase-bundle.js',  // Output bundle filename
+    filename: 'firebase-utils-bundle.js',  // Output bundle filename
     path: path.resolve(__dirname, 'dist'),  // Output directory (can change based on your setup)
     library: 'firebaseUtils',  // Global variable name when you include the bundle in a script
     libraryTarget: 'var',  // Defines how the library will be exposed
